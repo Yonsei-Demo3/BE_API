12ab73303cbc52a4a7baeca3193b2e5c436cb10a
leerura <sw09112@naver.com>
2025-11-02 15:19:29 +0900

    FEAT: 채팅방 참여 멤버 중간테이블 구현

---
+++ src/main/java/com/backend/RoomMember/RoomMember.java
@@ -0,0 +1,48 @@
+package com.backend.RoomMember;
+
+import com.backend.member.domain.Member;
+import com.backend.room.domain.Room;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "room_members", // 1. DB 테이블 이름 명시
+        uniqueConstraints = {
+                // 2. 한 유저가 한 방에 두 번 들어가는 것을 DB에서 원천 차단
+                @UniqueConstraint(
+                        name = "room_member_uk",
+                        columnNames = {"room_id", "member_id"}
+                )
+        }
+)
+@Getter
+@NoArgsConstructor
+public class RoomMember {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id", nullable = false )
+    private Room room;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+
+    //TODO: BaseEntity 구현 후 추가, User 역할 구분(방장 or 참가자)
+
+    @Builder
+    private RoomMember(Room room, Member member) {
+        this.room = room;
+        this.member = member;
+    }
+
+    public static RoomMember from(Room room, Member member) {
+        return new RoomMember(room, member);
+    }
+}
---
+++ src/main/java/com/backend/RoomMember/RoomMemberRepository.java
@@ -0,0 +1,7 @@
+package com.backend.RoomMember;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomMemberRepository extends JpaRepository<RoomMember,Long> {
+}
