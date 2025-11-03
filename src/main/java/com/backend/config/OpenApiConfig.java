3a56e918dde21ec024f880bca566074ac779fad9
우태호 <{>
2025-11-03 17:00:50 +0900

    [REFACTOR] 어노테이션 중복 제거 및 공통 정책 리팩토링

--- src/main/java/com/backend/config/OpenApiConfig.java
+++ src/main/java/com/backend/config/OpenApiConfig.java
@@ -2,7 +2,8 @@ package com.backend.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.*;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
