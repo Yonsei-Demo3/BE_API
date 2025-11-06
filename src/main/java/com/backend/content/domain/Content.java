217736fcec40d6f9eafb1407a43036f50eb2cbcb
leerura <sw09112@naver.com>
2025-11-06 23:33:06 +0900

    FIX: 컨트롤러 RequestBody 추가

--- src/main/java/com/backend/content/domain/Content.java
+++ src/main/java/com/backend/content/domain/Content.java
@@ -25,14 +25,22 @@ public class Content {
 
     @Column(name = "image_url", nullable = false, length = 1000)
     private String imageUrl;
+
+    @Column(length = 1000)
+    private String link;
     //TODO: Base Entity 연결, 카테고리 기능 구현 후 연결
 
     @Builder
-    private Content(String name, String creator, String description, String image_url) {
+    private Content(String name, String creator, String description, String image_url, String link) {
         this.name = name;
         this.creator = creator;
         this.description = description;
         this.imageUrl = image_url;
+        this.link = link;
+    }
+
+    public static Content of(String name, String creator, String description, String image_url, String link) {
+        return new Content(name, creator, description, image_url, link);
     }
 
 }
--- src/main/java/com/backend/question/controller/QuestionController.java
+++ src/main/java/com/backend/question/controller/QuestionController.java
@@ -9,10 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/questions")
@@ -22,7 +19,7 @@ public class QuestionController {
     private final QuestionService questionService;
 
     @PostMapping
-    public ResponseEntity<CreateQuestionResponseDTO> createFirstQuestion(@AuthenticationPrincipal CustomUserPrincipal me, CreateFirstQuestionRequestDTO dto) {
+    public ResponseEntity<CreateQuestionResponseDTO> createFirstQuestion(@AuthenticationPrincipal CustomUserPrincipal me, @RequestBody CreateFirstQuestionRequestDTO dto) {
         CreateQuestionResponseDTO responseDTO = questionService.createFirstQuestion(me.getUserId(), dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
     }
