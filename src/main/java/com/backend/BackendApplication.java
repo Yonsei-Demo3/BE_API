0837323bfa561d0be51e523ffee9a5fd9a1d526a
leerura <sw09112@naver.com>
2025-11-19 18:02:49 +0900

    CHORE: 배포환경 도커 컴포즈 따로 작성

--- src/main/java/com/backend/BackendApplication.java
+++ src/main/java/com/backend/BackendApplication.java
@@ -22,8 +22,21 @@ public class BackendApplication {
     @Bean
     public CommandLineRunner testRedis(NotificationPublisher notificationPublisher) {
         return args -> {
-            System.out.println("🚀 서버 시작되자마자 알림 발송 테스트!");
-            notificationPublisher.publish(NotificationMessage.of(999L, NotificationType.QUESTION_FULL));
+            // 별도의 스레드에서 실행 (메인 서버 부팅을 방해하지 않기 위함)
+            new Thread(() -> {
+                try {
+                    System.out.println("⏳ 서버 시작! 10초 뒤에 알림 발송 예정...");
+
+                    // 10초 대기 (10000ms)
+                    Thread.sleep(10000);
+
+                    System.out.println("🚀 10초 경과! 알림 발송!");
+                    notificationPublisher.publish(NotificationMessage.of(1L, NotificationType.QUESTION_FULL));
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start(); // 스레드 시작
         };
     }
 
