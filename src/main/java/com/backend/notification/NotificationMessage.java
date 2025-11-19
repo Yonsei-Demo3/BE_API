ef98d016db0312262b224e80d9755165ce0b73b7
leerura <sw09112@naver.com>
2025-11-19 16:17:51 +0900

    FEAT 알림 전송 함수 작성

---
+++ src/main/java/com/backend/notification/NotificationMessage.java
@@ -0,0 +1,10 @@
+package com.backend.notification;
+
+public record NotificationMessage(
+        Long receiverId,
+        NotificationType type
+) {
+    public static NotificationMessage from(Notification notification) {
+        return new NotificationMessage(notification.getReceiver().getId(),  notification.getType());
+    }
+}
--- src/main/java/com/backend/notification/NotificationPublisher.java
+++ src/main/java/com/backend/notification/NotificationPublisher.java
@@ -2,16 +2,29 @@ package com.backend.notification;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class NotificationPublisher {
 
-    private final StringRedisTemplate stringRedisTemplate;
+    private final StringRedisTemplate redisTemplate;
 
     private final ObjectMapper objectMapper;
 
-    private static final String TOPIC_NOTIFICATIONS = "notification-topic";
+    private static final String TOPIC_NOTIFICATION = "notification-topic";
+
+    public void publish(NotificationMessage message) {
+        try {
+            String jsonMessage = objectMapper.writeValueAsString(message);
+            redisTemplate.convertAndSend(TOPIC_NOTIFICATION, jsonMessage);
+            log.info("Published notification to topic: {}", jsonMessage);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
