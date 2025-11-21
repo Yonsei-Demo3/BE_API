7dbd0cd8d8231ba4cf53dc53a3fd44543a6aa884
우태호 <{>
2025-11-21 17:00:33 +0900

    [FEAT] 최근/인기 검색어 검색 엔진과 결합

--- src/main/java/com/backend/security/config/SecurityConfig.java
+++ src/main/java/com/backend/security/config/SecurityConfig.java
@@ -42,6 +42,8 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.GET, "/api/v1/questions/*/like").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/v1/questions/*/like").authenticated()
                 .requestMatchers(HttpMethod.DELETE, "/api/v1/questions/*/like").authenticated()
+                .requestMatchers("/api/v1/questions/search").permitAll()
+                .requestMatchers("/api/v1/search").permitAll()
                 .requestMatchers("/api/v1/contents/**").permitAll()
                 .requestMatchers("/api/admin/**").hasRole("ADMIN")             // 관리자 전용
                 .anyRequest().authenticated()
