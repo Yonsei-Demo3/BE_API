05e25c0ff435d0e73f252f55243e59116a87e706
우태호 <{>
2025-11-23 20:00:30 +0900

    [FIX] security config 질문 api close

--- src/main/java/com/backend/security/config/SecurityConfig.java
+++ src/main/java/com/backend/security/config/SecurityConfig.java
@@ -51,7 +51,6 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.POST, "/api/v1/members").permitAll()
 
                 // 질문
-                .requestMatchers(HttpMethod.POST, "/api/v1/questions/*").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/v1/questions/*").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/v1/questions/*/like").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/v1/questions/*/like").authenticated()
