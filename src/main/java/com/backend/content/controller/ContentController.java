aea37590a24f45d21edc51c623680b610bd8126d
우태호 <{>
2025-11-25 03:46:22 +0900

    [FEAT] 등록된 전체 콘텐츠 리스트 반환 api 구현

--- src/main/java/com/backend/content/controller/ContentController.java
+++ src/main/java/com/backend/content/controller/ContentController.java
@@ -2,11 +2,14 @@ package com.backend.content.controller;
 
 import com.backend.content.dto.ContentCreateRequestDTO;
 import com.backend.content.dto.ContentResponseDTO;
+import com.backend.content.dto.ContentWithCategoriesResponseDTO;
 import com.backend.content.service.ContentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/contents")
@@ -18,5 +21,12 @@ public class ContentController {
     public ResponseEntity<ContentResponseDTO> createContent(@RequestBody ContentCreateRequestDTO dto) {
         ContentResponseDTO responseDTO = contentService.createContent(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<ContentWithCategoriesResponseDTO>> getAllContents() {
+        return ResponseEntity.ok(
+                contentService.getAllContentsWithCategories()
+        );
     }
 }
---
+++ src/main/java/com/backend/content/dto/ContentCategoryDTO.java
@@ -0,0 +1,6 @@
+package com.backend.content.dto;
+
+public record ContentCategoryDTO(
+    String mainCategory,
+    String subCategory
+) {}
