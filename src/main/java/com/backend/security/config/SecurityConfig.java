57ee68b62276a1ffd268acd0a947333899ac654b
우태호 <{>
2025-11-25 18:51:10 +0900

    [FIX] cors에 petch 추가(닉네임 변경)

--- src/main/java/com/backend/security/config/SecurityConfig.java
+++ src/main/java/com/backend/security/config/SecurityConfig.java
@@ -104,7 +104,7 @@ public class SecurityConfig {
                 "https://talkwithsai.com"
         ));
 
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PETCH", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
 
         // ⭐ 여기에서 Authorization 헤더를 브라우저에 노출
