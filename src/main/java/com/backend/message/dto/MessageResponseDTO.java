ac249541ea2d5310de01621cf635529cf1b1cb59
leerura <sw09112@naver.com>
2025-11-26 10:08:42 +0900

    FEAT: 메시지 반환 dto에서 상태 및 작성 시간 추가

--- src/main/java/com/backend/message/dto/MessageResponseDTO.java
+++ src/main/java/com/backend/message/dto/MessageResponseDTO.java
@@ -1,6 +1,10 @@
 package com.backend.message.dto;
 
 import com.backend.message.domain.Message;
+import com.backend.message.domain.MessageType;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
 
 //TODO: DTO에 담는 내용 상세화
 public record MessageResponseDTO(
@@ -8,7 +12,11 @@ public record MessageResponseDTO(
         String content,
         Long senderId,
         String senderNickname,
-        boolean isMine
+        boolean isMine,
+        MessageType type,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+        LocalDateTime createdAt
+
 ) {
     public static MessageResponseDTO from(Message message, Long currentUserId) {
         boolean isMine = message.getMember().getId().equals(currentUserId);
@@ -17,7 +25,10 @@ public record MessageResponseDTO(
                 message.getContent(),
                 message.getMember().getId(),
                 message.getMember().getNickname(),
-                isMine);
+                isMine,
+                message.getType(),
+                message.getCreatedAt()
+        );
 
     }
 }
