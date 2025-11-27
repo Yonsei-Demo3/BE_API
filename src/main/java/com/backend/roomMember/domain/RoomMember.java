b1ad87ea1fa1084b7dc8391f44702912c4d3d775
우태호 <{>
2025-11-27 23:49:20 +0900

    [FEAT] 채팅방 참가 시 랜덤 닉네임 부여 및 중복 방지 기능 추가

--- src/main/java/com/backend/roomMember/domain/RoomMember.java
+++ src/main/java/com/backend/roomMember/domain/RoomMember.java
@@ -2,10 +2,13 @@ package com.backend.roomMember.domain;
 
 import com.backend.member.domain.Member;
 import com.backend.room.domain.Room;
+import com.backend.roomMember.util.RandomNicknameGenerator;
+
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import java.util.Set;
 
 @Entity
 @Table(name = "room_members", // 1. DB 테이블 이름 명시
@@ -37,22 +40,32 @@ public class RoomMember {
     @Column(nullable = false)
     private RoomMemberStatus roomMemberStatus;
 
+    @Column(name = "room_nickname", nullable = false)
+    private String roomNickname;
+
     public void doReady() {
         this.roomMemberStatus = RoomMemberStatus.READY;
     }
 
-
     //TODO: BaseEntity 구현 후 추가, User 역할 구분(방장 or 참가자)
 
     @Builder
-    private RoomMember(Room room, Member member, RoomMemberStatus status) {
+    private RoomMember(Room room, Member member, RoomMemberStatus status, String roomNickname) {
         this.room = room;
         this.member = member;
         this.roomMemberStatus = status;
+        this.roomNickname = roomNickname;
     }
 
     //처음엔 무조건 join으로 시작할것임 TODO: 네이밍 고려
-    public static RoomMember of(Room room, Member member) {
-        return new RoomMember(room, member, RoomMemberStatus.JOINED);
+    public static RoomMember of(Room room, Member member, Set<String> existingNicknames) {
+        String nickname = RandomNicknameGenerator.generate(existingNicknames);
+
+        return RoomMember.builder()
+                .room(room)
+                .member(member)
+                .status(RoomMemberStatus.JOINED)
+                .roomNickname(nickname)
+                .build();
     }
 }
